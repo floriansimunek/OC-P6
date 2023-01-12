12583efddfd2e70d7a83cc7e8f926701470f96e4
Florian SIMUNEK <florian.simunek@gmail.com>
2023-01-12 18:30:09 +0100

    :lipstick: Change styles to match figma file

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,4 @@
 function photographerFactory(data) {
-	console.log(data);
 	const { city, country, id, name, portrait, price, tagline } = data;
 
 	const picture = `assets/photographers/${portrait}`;
@@ -9,18 +8,23 @@ function photographerFactory(data) {
 
 		const img = document.createElement("img");
 		img.setAttribute("src", picture);
+		img.setAttribute("alt", name);
+		img.setAttribute("aria-label", "Voir le profil de " + name);
 
 		const h2 = document.createElement("h2");
 		h2.textContent = name;
 
 		const location = document.createElement("p");
 		location.textContent = city + ", " + country;
+		location.classList.add("location");
 
 		const slogan = document.createElement("p");
 		slogan.textContent = tagline;
+		slogan.classList.add("slogan");
 
 		const tjm = document.createElement("p");
 		tjm.textContent = price + "€/jour";
+		tjm.classList.add("price");
 
 		article.appendChild(img);
 		article.appendChild(h2);
