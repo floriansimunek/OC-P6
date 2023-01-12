7b19a52efab69e79e4ba501251f871b8d0e9d509
Florian SIMUNEK <florian.simunek@gmail.com>
2023-01-12 18:16:18 +0100

    :alien: get photographers datas from json file

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,34 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+	console.log(data);
+	const { city, country, id, name, portrait, price, tagline } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+	const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+	function getUserCardDOM() {
+		const article = document.createElement("article");
+
+		const img = document.createElement("img");
+		img.setAttribute("src", picture);
+
+		const h2 = document.createElement("h2");
+		h2.textContent = name;
+
+		const location = document.createElement("p");
+		location.textContent = city + ", " + country;
+
+		const slogan = document.createElement("p");
+		slogan.textContent = tagline;
+
+		const tjm = document.createElement("p");
+		tjm.textContent = price + "€/jour";
+
+		article.appendChild(img);
+		article.appendChild(h2);
+		article.appendChild(location);
+		article.appendChild(slogan);
+		article.appendChild(tjm);
+
+		return article;
+	}
+	return { name, picture, getUserCardDOM };
 }
