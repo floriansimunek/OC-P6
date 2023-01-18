505882e42c8abcaa6bfe3c54f81af538c68e28bc
Florian SIMUNEK <florian.simunek@gmail.com>
2023-01-18 21:56:25 +0100

    :wheelchair: make tab focus order

--- scripts/models/Photo.js
+++ scripts/models/Photo.js
@@ -6,10 +6,10 @@ class Photo extends Media {
 	createPhotoCard() {
 		return `
             <div class="media_block" id="media_${this.id}" data-title="${this.title}" data-date="${this.date}" data-likes="${this.likes}">
-                <img class="media image" src="${this.url}" alt="${this.title}">
+                <img tabindex="0" class="media image" src="${this.url}" alt="${this.title}">
                 <div class="media-informations">
-                    <p class="title">${this.title}</p>
-                    <p class="likes">${this.likes + this.svg}</p>
+                    <p class="title" tabindex="0">${this.title}</p>
+                    <p class="likes" tabindex="0">${this.likes}<span aria-hidden="true">${this.svg}</span></p>
                 </div>
             </div>
         `;
--- scripts/models/Video.js
+++ scripts/models/Video.js
@@ -6,12 +6,13 @@ class Video extends Media {
 	createVideoCard() {
 		return `
             <div class="media_block" id="media_${this.id}" data-title="${this.title}" data-date="${this.date}" data-likes="${this.likes}">
-                <video class="media video">
+                <video class="media video" tabindex="0">
                     <source src="${this.url}" type="video/mp4">
+                    <meta itemprop="title" content="${this.title}">
                 </video>
                 <div class="media-informations">
-                    <p class="title">${this.title}</p>
-                    <p class="likes">${this.likes + this.svg}</p>
+                    <p class="title" tabindex="0">${this.title}</p>
+                    <p class="likes" tabindex="0">${this.likes}<span aria-hidden="true">${this.svg}</span></p>
                 </div>
             </div>
         `;
